c17bf85783912ee4dabab411691fffc16f67bd97
nirmala-vibgyour <nirmalawrk@gmail.com>
2025-06-05 17:40:24 +0530

    made changes in the db config

--- config/db.js
+++ config/db.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_URL,
     {
-        host:process.env.DB_HOST,
-        port:process.env.DB_PORT || 3306,
         dialect:'mysql',
         logging:false,
     }
